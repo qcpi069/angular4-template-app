987a481c504a9ad3c0685f65b51a5f7c5d2ebc30
= <jose.a.deguzman@gmail.com>
2018-01-30 11:47:30 -0600

    Adds navigation component stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
