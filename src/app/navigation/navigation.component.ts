4fe42b3c4c8e820d201592617d31cb0b6cb3aa96
qcpi069 <jose.a.deguzman@gmail.com>
2018-01-30 19:42:12 -0600

    Adds stubs for navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  lastNavigation: String;
+  recentNavigation: String;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  navigateHome(){
+      //implement functionality to display home contents here
+      this.logNavigationHistory("Home");
+      
+  }
+
+  navigateToFinder(){
+    //implement functionality to display finder contents here
+    this.logNavigationHistory("Finder");
+  }
+
+  private logNavigationHistory(currentNavigation: String){
+    //log current and previous navigation history -- can also build dynamic call tree for 'bread-crumbs'
+    this.lastNavigation = new String(this.recentNavigation);
+    this.recentNavigation = new String(currentNavigation);
+    console.log("navigating to "+currentNavigation+" previous navigation was "+this.lastNavigation);
   }
 
 }
